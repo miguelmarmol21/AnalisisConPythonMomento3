6ecce66e4d43410e06fc2ab215aa80f5aca2a6bd
miguelmarmol21 <miguelmarmol21@outlook.es>
2023-06-12 22:54:05 -0500

    Se realizan graficas para los municipios

--- helpers/crearBarra.py
+++ helpers/crearBarra.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 def graficarQuery(dataFrame,campoX,campoY,nombreGrafica):
-    colores=['green','red','blue']
+    colores=['#2A5C30', '#185286', '#9E6114', '#49BB0D', '#BB0D15']
     promedio=dataFrame.groupby(campoX)[campoY].mean()
     
     #Generar la grafica
@@ -9,6 +9,6 @@ def graficarQuery(dataFrame,campoX,campoY,nombreGrafica):
     plt.bar(promedio.index,promedio,color=colores)
     plt.title('Promedio de Siembra')
     plt.xlabel('Veredas')
-    plt.ylabel('Arboles')
+    plt.ylabel('Arboles Sembrados')
     
     plt.savefig(f"./assets/img/{nombreGrafica}.png")
--- main.py
+++ main.py
@@ -2,22 +2,44 @@ import pandas as pd
 from helpers.arbolesAnt import crearTabla
 from helpers.crearBarra import graficarQuery
 
+# Crear el dataFrame
+
 tabla=pd.read_csv("./data/Siembras.csv",delimiter=";")
 
-# arboleSembrados=tabla.query('Ciudad == "Santa Fe de Antioquia" and Arboles > 250')
-# queryCaucasia=tabla.query('Ciudad == "Caucasia"')
-# queryBelmira=tabla.query('Vereda == "Rio Arriba" or Vereda == "La Salazar"')
+# Crear los query de los filtros
+
+querysantaFeAntioquia=tabla.query('Ciudad == "Santa Fe de Antioquia" and Arboles > 250')
+queryCaucasia=tabla.query('Ciudad == "Caucasia"')
+queryBelmira=tabla.query('Vereda == "Rio Arriba" or Vereda == "La Salazar"')
 queryBello=tabla.query('Ciudad == "Bello" and Vereda == "Quitasol"')
-# queryCaramanta=tabla.query('Ciudad == "Caramanta" and Arboles > 100')
-# queryYarumal=tabla.query('Vereda == "Mallarino"')
+queryCaramanta=tabla.query('Ciudad == "Caramanta" and Arboles > 100')
+queryYarumal=tabla.query('Vereda == "Mallarino"')
 
-# crearTabla(arboleSembrados,"arbolesAnt")
-# crearTabla(queryCaucasia,"caucasia")
-# crearTabla(queryBelmira,"veredasBelmira")
-# crearTabla(queryBello,"veredaQuitasolBello")
-# crearTabla(queryCaramanta,"arbolesMayoresCaramanta")
-# crearTabla(queryYarumal,"veredaMallaridoYarumal")
+# Crear las tablas de los query
+
+crearTabla(querysantaFeAntioquia,"arbolesSantaFeAnt")
+crearTabla(queryCaucasia,"caucasia")
+crearTabla(queryBelmira,"veredasBelmira")
+crearTabla(queryBello,"veredaQuitasolBello")
+crearTabla(queryCaramanta,"arbolesMayoresCaramanta")
+crearTabla(queryYarumal,"veredaMallaridoYarumal")
 crearTabla(queryBello.describe(),"veredaQuitasolBello")
-# estadisticaBello = queryBello.describe()
 
-# print(estadisticaBello)
+# Crear los query de cada ciudad con Arboles sembrados mayores a 500
+
+queryGraficaSantaFeAnt=tabla.query('Ciudad == "Santa Fe de Antioquia" and Arboles > 500')
+queryGraficaCaucasia=tabla.query('Ciudad == "Caucasia" and Arboles > 500')
+queryGraficaBelmira=tabla.query('Ciudad == "Belmira" and Arboles > 500')
+queryGraficaBello=tabla.query('Ciudad == "Bello" and Arboles > 500')
+queryGraficaCaramanta=tabla.query('Ciudad == "Caramanta" and Arboles > 500')
+queryGraficaYarumal=tabla.query('Ciudad == "Yarumal" and Arboles > 500')
+
+# Graficar los query realizados para cada municipio
+
+graficarQuery(queryGraficaSantaFeAnt,'Vereda','Arboles','graficaSantafeAnt')
+graficarQuery(queryGraficaCaucasia,'Vereda','Arboles','graficaCaucasia')
+graficarQuery(queryGraficaBelmira,'Vereda','Arboles','graficaBelmira')
+graficarQuery(queryGraficaBello,'Vereda','Arboles','graficaBello')
+graficarQuery(queryGraficaCaramanta,'Vereda','Arboles','graficaCaramanta')
+graficarQuery(queryGraficaYarumal,'Vereda','Arboles','graficaYarumal')
+
